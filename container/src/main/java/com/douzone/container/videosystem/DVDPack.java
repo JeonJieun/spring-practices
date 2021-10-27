0b83b3c63e66d173ef46075b8d1adb05c22f7997
JeonJieun <wjn135@naver.com>
2021-10-27 16:25:45 +0900

    20211027수업

--- container/src/main/java/com/douzone/container/videosystem/DVDPack.java
+++ container/src/main/java/com/douzone/container/videosystem/DVDPack.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class DVDPack {
 	private String title;
 	private List<DigitalVideoDisc> dvds;
-
+	
 	public DVDPack(String title, List<DigitalVideoDisc> dvds) {
 		this.title = title;
 		this.dvds = dvds;
